c7669dfa9dd0a44bd698f22c8854bf4373d21216
Ellis <espellerberg@gmail.com>
2017-04-27 13:41:29 -0700

    Updated Main Screen Java

--- app/src/main/java/com/example/ellis/haxddesign/MainScreen.java
+++ app/src/main/java/com/example/ellis/haxddesign/MainScreen.java
@@ -2,12 +2,22 @@ package com.example.ellis.haxddesign;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainScreen extends AppCompatActivity {
+    private Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+        start = (Button) findViewById(R.id.buttonStart);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Move to Target Select
+            }
+        });
     }
 }
